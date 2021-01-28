581c01236c9cd3d601459781abdb154a1ac6abd8
Daniel Salazar <daniel.salazarop@gmail.com>
2021-01-27 18:50:31 -0600

    Update settings.py

--- proyecto_web/settings.py
+++ proyecto_web/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(ywbaxf5%5(zl2u67d7d2)!)2srvmzofdl2_7*grfu3b!q99ds'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
